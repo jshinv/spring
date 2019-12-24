2b1184cb627d9e152545033cfc04b6902400414f
Juhyun Shin <jshinv@gmail.com>
2019-12-24 11:28:51 +0900

    Add spring

---
+++ basic/src/test/java/com/jshinv/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jshinv.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
