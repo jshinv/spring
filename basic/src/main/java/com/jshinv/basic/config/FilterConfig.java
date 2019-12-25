d9a55c11cd49efce8dd1ca0b689844f1f7bf373b
Juhyun Shin <jshinv@gmail.com>
2019-12-25 12:34:59 +0900

    add h2

---
+++ basic/src/main/java/com/jshinv/basic/config/FilterConfig.java
@@ -0,0 +1,19 @@
+package com.jshinv.basic.config;
+
+import javax.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.jshinv.basic.filter.IPCheckFilter;
+
+
+@Configuration
+public class FilterConfig {
+	@Bean
+	public FilterRegistrationBean<Filter> getFilterRegistrationBean() {
+		FilterRegistrationBean<Filter> bean = new FilterRegistrationBean<>(new IPCheckFilter());
+		bean.addUrlPatterns("/visitor");
+		bean.addUrlPatterns("/main");
+		return bean;
+	}
+}
